acdd7683b670c7482e01dfd864d578ba6b8387ca
Himala <himala76@gmail.com>
2016-08-25 19:15:47 -0400

    second day class

---
+++ Practice _01 _arithmetic.py
@@ -0,0 +1,14 @@
+#Himala
+
+print "Testing Python Basic Arithmetic Operation "
+multi_a = int(raw_input("Enter first number for test "))
+multi_b = int(raw_input("Enter second number for test "))
+print""
+print "Answer for Addition"
+print multi_a + multi_b
+print""
+print "Answer for Subtraction"
+print multi_a - multi_b
+print""
+print "Answer for Multiplication"
+print multi_a * multi_b
