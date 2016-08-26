cb729fec1e7a216231c65eeb3502230dc23a9753
Himala <himala76@gmail.com>
2016-08-25 20:03:30 -0400

    second day class

---
+++ Random_randint_module.py
@@ -0,0 +1,7 @@
+#testing random module in python
+
+# this codes show how to use the random module in python
+
+import random
+random_number = random.randint(-10,10)
+print "The number is: " + str(random_number)
