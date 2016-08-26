2c5b61aea825bf5fb5f47f9c03bcb01f69a0d33c
Himala <himala76@gmail.com>
2016-08-26 11:31:06 -0400

    randon game update1

--- random_number_game.py
+++ random_number_game.py
@@ -1,4 +1,5 @@
 #program by himala
+#date 8/26/2016
 
 
 print "******* Welcome to Number Game *********"
@@ -9,11 +10,16 @@ print ""
 print "Please Enter Game Level you like to paly"
 game_level = int(raw_input("Level 1 (Easy) enter 1, Level 2 (Intermediate) enter 2, Level 3 (Difficult) enter 3 : "))
 print ""
+print "Good Luck"
+print ""
 #Level 1
 if game_level == 1:
     player_number = raw_input("enter number between 1 to 5 : ")
     import random
     random_number = random.randint(1, 5)
+    import time
+    print "Wait Computer Thinking"
+    time.sleep(1.5)
     print "Computer says: " + str(random_number)
     if (random_number == player_number):
         print "****** you win ******"
@@ -24,6 +30,9 @@ elif game_level == 2:
     player_number = raw_input("enter number between 1 to 25 : ")
     import random
     random_number = random.randint(1, 25)
+    import time
+    print "Wait Computer Thinking"
+    time.sleep(1.5)
     print "Computer says: " + str(random_number)
     if (random_number == player_number):
         print "****** you win ******"
@@ -33,6 +42,9 @@ elif game_level == 2:
 elif game_level == 3:
     player_number = raw_input("enter number between 1 to 50 : ")
     import random
+    import time
+    print "Wait Computer Thinking"
+    time.sleep(1.5)
     random_number = random.randint(1, 50)
     print "Computer says: " + str(random_number)
     if (random_number == player_number):
