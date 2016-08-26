a1ef54e2b63dd038ccb19b7d8785e7cafd1fc043
Himala <himala76@gmail.com>
2016-08-26 14:46:31 -0400

    randon game update1

--- random_number_game.py
+++ random_number_game.py
@@ -1,6 +1,8 @@
 #program by himala
 #date 8/26/2016
 
+import random
+import time
 
 print "******* Welcome to Number Game *********"
 print "How to paly:  Enter your number if computer guess same as your number then you will win "
@@ -8,16 +10,14 @@ print ""
 
 #game levels
 print "Please Enter Game Level you like to paly"
-game_level = int(raw_input("Level 1 (Easy) enter 1, Level 2 (Intermediate) enter 2, Level 3 (Difficult) enter 3 : "))
+game_level = raw_input("Level 1 (Easy) enter 1, Level 2 (Intermediate) enter 2, Level 3 (Difficult) enter 3 : ")
 print ""
 print "Good Luck"
 print ""
 #Level 1
-if game_level == 1:
+if game_level == "1":
     player_number = raw_input("enter number between 1 to 5 : ")
-    import random
     random_number = random.randint(1, 5)
-    import time
     print "Wait.. Computer Thinking"
     time.sleep(1.5)
     print "Computer says: " + str(random_number)
@@ -26,11 +26,9 @@ if game_level == 1:
     else:
         print " sorry you lost :( "
 #Level 2
-elif game_level == 2:
+elif game_level == "2":
     player_number = raw_input("enter number between 1 to 25 : ")
-    import random
     random_number = random.randint(1, 25)
-    import time
     print "Wait.. Computer Thinking"
     time.sleep(1.5)
     print "Computer says: " + str(random_number)
@@ -39,10 +37,8 @@ elif game_level == 2:
     else:
         print " sorry you lost :( "
 #Level 3
-elif game_level == 3:
+elif game_level == "3":
     player_number = raw_input("enter number between 1 to 50 : ")
-    import random
-    import time
     print "Wait.. Computer Thinking"
     time.sleep(1.5)
     random_number = random.randint(1, 50)
